0b99c2d969cfc954a79a39217a8af3fc32f7f07a
gihantha <gihantha01@gmail.com>
2024-12-07 01:35:46 +0530

    feat: integrate admin template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,14 +1,19 @@
-<x-app-layout>
-    
-</x-app-layout>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
-    <title>Document</title>
+    @include('admin.css')
 </head>
+
 <body>
-    admin
+
+    @include('admin.sidebar')
+
+    @include('admin.header')
+
+    @include('admin.body')
+
+    @include('admin.script')
 </body>
+
 </html>
