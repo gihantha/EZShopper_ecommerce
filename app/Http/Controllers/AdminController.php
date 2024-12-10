ab51d7fd43d8a6ba1a258408d236470f3463ace7
gihantha <gihantha01@gmail.com>
2024-12-10 16:11:07 +0530

    feat(product):added product via form

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Models\Category;
 
+use App\Models\Product;
+
+
 
 
 class AdminController extends Controller
@@ -36,4 +39,30 @@ class AdminController extends Controller
         return redirect()->back()->with('message','Category Deleted Successfully');
     }
 
+    public function view_product()
+    {
+        $category =  category::all();
+        return view('admin.product', compact('category'));
+    }
+
+    public function add_product(Request $request)
+    {
+        $product=new product;
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->discount_price = $request->dis_price;
+        $product->category = $request->category;
+        $image = $request->image;
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product', $imagename);
+        $product->image = $imagename;
+
+
+        $product->save();
+
+        return redirect()->back()->with('message','Product Added Successfully');
+    }
 }
