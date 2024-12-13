b1aa351224fb0ec6f13d79ac854aae51530184e6
gihantha <gihantha01@gmail.com>
2024-12-13 11:09:43 +0530

    feat(UserPage):show product data in user page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,12 +7,15 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 
+use App\Models\Product;
+
 class HomeController extends Controller
 {
 
     public function index()
     {
-        return view('home.userpage');
+        $product = Product::paginate(10);
+        return view('home.userpage', compact('product'));
     }
     public function redirect()
     {
@@ -24,7 +27,8 @@ class HomeController extends Controller
         }
         else
         {
-            return view('home.userpage');
+            $product = Product::paginate(10);
+            return view('home.userpage', compact('product'));
         }
     }
 }
